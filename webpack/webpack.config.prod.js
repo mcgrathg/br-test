da69f386890c5d2ef8de95ca3a5eff0ba3dd6277
Greg McGrath <Greg.McGrath@contractor.cengage.com>
2019-03-19 00:02:44 -0400

    copy images to build dir

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = require('./webpack.config.base.js');
 
@@ -10,6 +11,10 @@ module.exports = merge(baseConfig, {
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
     }),
+
+    new CopyPlugin([
+      { from: 'app/assets/images/Cuts', to: 'assets/images/Cuts' },
+    ]),
   ],
   externals: {
     react: 'React',
